b57470eb0a47e4a2b51efa3d16d44cd5c1d28c27
Rachel Micolichek <Waachel@aol.com>
2025-07-14 23:08:04 -0700

    Adding whale animation to scene 2
    Hover, forward, forward faster, ascend, descend, turn still needs a little work

--- Assets/UI/SceneLoader.cs
+++ Assets/UI/SceneLoader.cs
@@ -3,17 +3,47 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [Header("Scene Settings")]
+    public string GameScene = "GameScene";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            LoadNextScene();
+        }
     }
+
+    void LoadNextScene()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        switch (currentScene)
+        {
+            case "Homepage":
+                SceneManager.LoadScene("Scene2");
+                break;
+
+            case "Scene4":
+                SceneManager.LoadScene("GameScene");
+                break;
+
+            case "PersistentUI":
+                SceneManager.LoadScene("Homepage");
+                break;
+
+            default:
+                Debug.LogWarning($"No scene transition defined for current scene: {currentScene}");
+                break;
+        }
+    }
+
     public void LoadTargetScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
